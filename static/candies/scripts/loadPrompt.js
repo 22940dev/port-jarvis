9b2a9f9f692f6c1067bf2da3759d38d59e022c25
Jake Jarvis <jakejarvis@gmail.com>
2019-01-16 11:54:33 -0500

    cleaning up random folders already hosted elsewhere

--- static/candies/scripts/loadPrompt.js
+++
@@ -1,7 +0,0 @@
-function loadPrompt() {
-    var code = prompt("Please enter your five-character code.", "");
-    
-    if(code != null) {
-        window.location = 'index.php?pass=' + code;
-    }
-}
