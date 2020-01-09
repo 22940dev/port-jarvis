5b44ee101fafc9a62ff124d5324c94f7453b29d9
Jake Jarvis <jake@jarv.is>
2020-01-09 09:20:56 -0500

    use postcss via hugo pipes instead of cli

---
+++ postcss.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  options: {
+    map: false
+  },
+  plugins: [
+    require('autoprefixer')(),
+    require('postcss-clean')({
+      compatibility: '*',
+      level: 0,
+      format: {
+        breaks: {
+          afterAtRule: true,
+          afterBlockBegins: true,
+          afterBlockEnds: true,
+          afterComment: true,
+          afterRuleEnds: true,
+          beforeBlockEnds: true
+        },
+        spaces: {
+          beforeBlockBegins: true
+        },
+        semicolonAfterLastProperty: true
+      }
+    })
+  ]
+};
