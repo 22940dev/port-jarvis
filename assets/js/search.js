54357538d38c0d78b405a3124342ba6ebf441092
Jake Jarvis <jake@jarv.is>
2020-12-19 11:41:04 -0500

    bump Hugo to v0.79.1

---
+++ assets/js/search.js
@@ -0,0 +1,7 @@
+algoliasearchNetlify({
+  appId: "0XNKMPBIAW",
+  apiKey: "2cdf280c8d3ba1b40c38a7d71bbe5b0b",
+  siteId: "a7403a53-fd9d-44c0-a708-a84d9fc1454d",
+  branch: "main",
+  selector: "div#search",
+});
