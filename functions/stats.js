78518fe326fa74a0e8f9120445c7e591de4e5747
Jake Jarvis <jake@jarv.is>
2021-05-31 22:29:50 -0400

    move /api/popular to /api/stats, calculate totals

--- functions/stats.js
+++ functions/stats.js
@@ -7,6 +7,9 @@ const pluralize = require("pluralize");
 const rssParser = require("rss-parser");
 require("dotenv").config();
 
+const baseUrl = "https://jarv.is/";
+const feedUrl = "https://jarv.is/feed.xml";
+
 exports.handler = async () => {
   try {
     const parser = new rssParser({
@@ -16,7 +19,7 @@ exports.handler = async () => {
       secret: process.env.FAUNADB_SERVER_SECRET,
     });
 
-    const feed = await parser.parseURL("https://jarv.is/feed.xml");
+    const feed = await parser.parseURL(feedUrl);
     const result = await client.query(
       q.Map(
         q.Paginate(q.Documents(q.Collection("hits"))),
@@ -24,28 +27,41 @@ exports.handler = async () => {
       )
     );
 
-    let posts = result.data;
+    let stats = {
+      total: {
+        hits: 0,
+      },
+      pages: result.data,
+    };
 
-    posts.map((p) => {
+    stats.pages.map((p) => {
       // match URLs from RSS feed with db to populate some metadata
-      let match = feed.items.find((x) => x.link === "https://jarv.is/" + p.slug + "/");
+      let match = feed.items.find((x) => x.link === baseUrl + p.slug + "/");
       if (match) {
         p.title = match.title;
         p.url = match.link;
         p.date = match.isoDate;
+        delete p.slug;
       }
 
-      // add comma-separated numbers and proper pluralization on the backend
+      // it's easier to add comma-separated numbers and proper pluralization here on the backend
       p.pretty_hits = numeral(p.hits).format("0,0");
       p.pretty_unit = pluralize("hit", p.hits);
+
+      // add these hits to running tally
+      stats.total.hits += p.hits;
 
       return p;
     });
 
     // sort by hits (descending)
-    posts.sort((a, b) => {
+    stats.pages.sort((a, b) => {
       return a.hits > b.hits ? -1 : 1;
     });
+
+    // do same prettification as above to totals
+    stats.total.pretty_hits = numeral(stats.total.hits).format("0,0");
+    stats.total.pretty_unit = pluralize("hit", stats.total.hits);
 
     return {
       statusCode: 200,
@@ -54,7 +70,7 @@ exports.handler = async () => {
         "Access-Control-Allow-Methods": "GET",
         "Access-Control-Allow-Origin": "*",
       },
-      body: JSON.stringify(posts),
+      body: JSON.stringify(stats),
     };
   } catch (error) {
     console.error(error);
